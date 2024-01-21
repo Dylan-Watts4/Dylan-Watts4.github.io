217ce7bf4596532d23f5f48de2eee4d827a60f2a
Jimmy-Jenga <dylanuk2001@hotmail.co.uk>
2024-01-21 20:35:57 +0000

    Update layout and styling for About Me and Top 5 Programming Languages sections

--- scripts/get-code-stats.js
+++ scripts/get-code-stats.js
@@ -12,24 +12,38 @@ fetch("https://api.github.com/users/Dylan-Watts4/repos")
                 allLanguages[language] = (allLanguages[language] || 0) + bytes;
             }
         });
-        // TODO: Sort languages by bytes
+        languagesArray = Object.entries(allLanguages);
+        languagesArray.sort((a, b) => b[1] - a[1]);
+        const top5Languages = languagesArray.slice(0, 5);
+        const top5LanguagesObject = Object.fromEntries(top5Languages);
         const ctx = document.getElementById('code-stats').getContext('2d');
         new Chart(ctx, {
             type: 'pie',
             data: {
-                labels: Object.keys(allLanguages),
+                labels: Object.keys(top5LanguagesObject),
                 datasets: [{
-                    data: Object.values(allLanguages),
+                    data: Object.values(top5LanguagesObject),
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)', // Red
+                        'rgba(54, 162, 235, 0.2)', // Blue
+                        'rgba(255, 206, 86, 0.2)', // Yellow
+                        'rgba(75, 192, 192, 0.2)', // Green
+                        'rgba(153, 102, 255, 0.2)', // Purple
                     ]
                 }]
             },
             options: {
                 responsive: false,
                 maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        labels: {
+                            color: 'rgb(211, 206, 206)'
+                        }
+                    }
+                },
                 title: {
-                    display: true,
+                    display: false,
                     text: 'Programming Languages Used'
                 }
             }
